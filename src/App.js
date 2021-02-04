62267a13a29053082cb687adc4a6b0458aab463d
Sabbha <sabbha.power1@gmail.com>
2021-02-04 23:34:08 +0530

    Initial commit of website, using create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Homepage from './homepage.components';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Homepage />
+			</div>
+		);
+	}
+}
+
+export default App;
